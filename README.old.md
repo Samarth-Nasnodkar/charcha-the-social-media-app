6655f7688b89ef5966c1b61210fff5544b6902a6
Samarth Nasnodkar <samarthnasnodkar@gmail.com>
2024-05-12 21:47:33 +0530

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# charcha-the-social-media-app
+ This is a full-stack social media app I'm developing!
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import {Header} from "./Header";
+import {Body} from "./Body";
+
+export function Home() {
+    return (
+        <div className='home-main'>
+            <Header />
+            <Body  />
+        </div>
+    )
+}
